a58efac15a08b8ee351d203eb80866149ebe4bb6
o-agustinSilva <agustinsilva1009@gmail.com>
2023-05-18 14:31:37 -0300

    Se agregaron los modelos

--- pagina_web/models.py
+++ pagina_web/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Director(models.Model):
+    nombre          = models.CharField(max_length=50)
+    nacionalidad    = models.CharField(max_length=30)
+    # foto 
+    anio_nacimiento = models.IntegerField(default = None)
+    resumen_bibliografico = models.CharField(max_length=300)
+    # Declarar clave foranea
+    
+
+class Actor(models.Model):
+    pass
+
+class Pelicula(models.Model):
+    pass
+
