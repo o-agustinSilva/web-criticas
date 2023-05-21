58eaba9fb2965ade27133367585c7fae931e5d3c
o-agustinSilva <agustinsilva1009@gmail.com>
2023-05-21 20:06:42 -0300

    Versión estable, agregados los divs con sus posibles diseños

--- pagina_web/models.py
+++ pagina_web/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from faker import Faker # CMD: pip install faker 
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password, check_password
+import random
 
 # ----------- Clases -----------
 class Director(models.Model):
@@ -23,16 +26,64 @@ class Actor(models.Model):
 
 class Pelicula(models.Model):
 
+    # Choices
+    CATEGORIAS_PELICULAS = [
+                                ("ACCION", 'Accion'),
+                                ("COMEDIA", 'Comedia'), 
+                                ("DRAMA", 'Drama'), 
+                                ("AVENTURA", 'Aventura'),
+                                ("TERROR", 'Terror'),
+                                ("CIENCIA FICCION", "Ciencia ficción")
+                           ]   
+
     # Datos de la pelicula
-    nombre          = models.CharField(max_length=70)
-    resumen         = models.CharField(max_length=1000)
     anio_pelicula   = models.IntegerField(default = None)
+    nombre          = models.CharField(max_length = 70)
+    resumen         = models.CharField(max_length = 1000)
+    categoria       = models.CharField(
+                                        max_length  = 20, 
+                                        choices     = CATEGORIAS_PELICULAS,
+                                        default     = "ACCION",
+                                      )  
 
     # Claves foráneas
     director        = models.ForeignKey(Director, on_delete=models.CASCADE)
     actores         = models.ManyToManyField(Actor)
 
+class Usuario(models.Model):
 
+    # Choices
+    ROLES_USUARIOS = [
+                        ("USUARIO", "Usuario"),
+                        ("ADMINISTRADOR", "Administrador"),
+                     ] 
+
+    # Datos de los usuarios
+    usuario     = models.CharField(max_length = 15)
+    contrasenia = models.CharField(max_length = 20)
+    rol         = models.CharField(
+                                    max_length  = 13,
+                                    choices     = ROLES_USUARIOS,
+                                    default     = "USUARIO",
+                                  )
+    
+    def set_password(self, raw_password):
+        # Lógica para establecer y almacenar la contraseña de manera segura
+        self.contrasenia = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        # Lógica para verificar la contraseña ingresada por el usuario
+        return check_password(raw_password, self.contrasenia)
+    
+class Critica(models.Model):
+    
+    #Datos de las criticas
+    critica     = models.CharField(max_length = 1000)
+    valoracion  = models.IntegerField(default = None)
+    usuario     = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    pelicula    = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+
+'''
 # ----------- Managers -----------
 class DirectorManager(models.Manager):
     def crear_nombre(self):
@@ -46,7 +97,12 @@ class DirectorManager(models.Manager):
         nacionalidad    = faker.country()
 
         return nacionalidad
+    
+    def crear_anio(self):
+        anio = random.randint(1930, 2023)
+        return anio
        
-    def crear_autor(self, cantidad):
+    def crear_director(self, cantidad):
         for i in range(cantidad):
-            self.create(nombre="Autor " + str(i), nacionalidad = self.crear_nacionalidad(), fecha_nacimiento = self.crear_fecha(i))
+            self.create(nombre= self.crear_nombre(), nacionalidad = self.crear_nacionalidad(), anio_nacimiento = self.crear_anio())
+'''
--- pagina_web/views.py
+++ pagina_web/views.py
@@ -1,3 +1,11 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class Inicio(TemplateView):
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+      context = super().get_context_data(**kwargs)
+      return context
